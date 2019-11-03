90390cff68c3c537355803c2644ceecaac8dde37
JUNLEE <leejun6694@gmail.com>
2019-11-03 17:33:54 +0900

    [BEEIOS-2] Initialize the project

---
+++ Morningbees-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Morningbees-iOS
+//
+//  Created by JUN LEE on 2019/11/03.
+//  Copyright © 2019 JUN LEE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
