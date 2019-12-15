ee95a9a07c7e10b179a556613829c92ed5e4d9e7
JUNLEE <leejun6694@gmail.com>
2019-12-15 23:47:47 +0900

    [BEEIOS-9] Add the swiftlint rule and Fix the lint warning

--- Morningbees-iOS/SignIn/ViewController/SignInViewController.swift
+++ Morningbees-iOS/SignIn/ViewController/SignInViewController.swift
@@ -12,18 +12,18 @@ import NaverThirdPartyLogin
 import UIKit
 
 final class SignInViewController: UIViewController {
-    
+
 //MARK:- Properties
-    
+
     private let naverSignInInstance = NaverThirdPartyLoginConnection.getSharedInstance()
-    
+
 //MARK:- Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         GIDSignIn.sharedInstance()?.presentingViewController = self
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.navigationBar.isHidden = true
@@ -33,7 +33,7 @@ final class SignInViewController: UIViewController {
 //MARK:- Navigation Control
 
 extension SignInViewController {
-    
+
     private func pushToSignUpViewController() {
         guard let signUpViewController = self.storyboard?.instantiateViewController(
             identifier: "SignUpViewController") as? SignUpViewController else {
@@ -47,26 +47,26 @@ extension SignInViewController {
 //MARK:- SignIn with Naver
 
 extension SignInViewController: NaverThirdPartyLoginConnectionDelegate {
-    
+
     func oauth20ConnectionDidFinishRequestACTokenWithAuthCode() {
         print("[Success] : SignIn with Naver")
         pushToSignUpViewController()
     }
-    
+
     func oauth20ConnectionDidFinishRequestACTokenWithRefreshToken() {
         print("[Success] : refresh token")
         pushToSignUpViewController()
     }
-    
+
     func oauth20ConnectionDidFinishDeleteToken() {
     }
-    
+
     func oauth20Connection(_ oauthConnection: NaverThirdPartyLoginConnection!, didFailWithError error: Error!) {
         print("[Error] :", error.localizedDescription)
     }
-    
+
     //MARK: Action
-    
+
     @IBAction private func touchUpSignInNaver(_ sender: UIButton) {
         naverSignInInstance?.delegate = self
         naverSignInInstance?.requestThirdPartyLogin()
@@ -76,11 +76,11 @@ extension SignInViewController: NaverThirdPartyLoginConnectionDelegate {
 //MARK:- SignIn with Google
 
 extension SignInViewController {
-    
+
     //MARK: Action
-    
+
     @IBAction private func touchUpSignInGoogle(_ sender: UIButton) {
-        if (GIDSignIn.sharedInstance()?.hasPreviousSignIn() ?? false) {
+        if GIDSignIn.sharedInstance()?.hasPreviousSignIn() ?? false {
             GIDSignIn.sharedInstance()?.restorePreviousSignIn()
         } else {
             GIDSignIn.sharedInstance()?.signIn()
