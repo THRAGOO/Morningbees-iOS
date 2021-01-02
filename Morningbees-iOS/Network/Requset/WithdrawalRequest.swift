82df2f5b6181d516cab28f20aebaec0afbaf8d86
Koo <31850425+gen-com@users.noreply.github.com>
2021-01-02 12:41:57 +0900

    [BEEIOS-52] Withdrawal Request

---
+++ Morningbees-iOS/Network/Requset/WithdrawalRequest.swift
@@ -0,0 +1,37 @@
+//
+//  WithdrawalRequest.swift
+//  Morningbees-iOS
+//
+//  Created by Byeongjo Koo on 2020/11/11.
+//  Copyright © 2020 THRAGOO. All rights reserved.
+//
+
+import Foundation
+
+final class WithdrawalAPI {
+}
+
+extension WithdrawalAPI {
+    
+    func request(completion: @escaping (Bool?, Error?) -> Void) {
+        let reqModel = WithdrawalModel()
+        let request = RequestSet(method: reqModel.method, path: reqModel.path)
+        let withdrawalRequest = Request<Withdrawal>()
+        KeychainService.extractKeyChainToken { (accessToken, _, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            guard let accessToken = accessToken else {
+                return
+            }
+            let headers: [String: String] = [RequestHeader.accessToken.rawValue: accessToken]
+            withdrawalRequest.request(req: request, header: headers, param: "") { (_, error) in
+                if let error = error {
+                    completion(nil, error)
+                }
+                UserDefaults.standard.removeObject(forKey: "beeID")
+                completion(true, nil)
+            }
+        }
+    }
+}
