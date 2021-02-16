69ad33369a9264de926212544d6c73847f937118
Koo <31850425+gen-com@users.noreply.github.com>
2021-02-16 20:10:46 +0900

    [BEEIOS-64] Extra Navigation Handling

--- Morningbees-iOS/NavigationControl/NavigationControl.swift
+++ Morningbees-iOS/NavigationControl/NavigationControl.swift
@@ -9,111 +9,20 @@
 import UIKit
 
 final class NavigationControl {
-    let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+    
+    static let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
 }
 
 extension NavigationControl {
     
-    func popToRootViewController() {
+    static func popToRootViewController() {
         DispatchQueue.main.async {
             let navigationController = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController
             navigationController?.popToRootViewController(animated: true)
         }
     }
     
-    func popToPrevViewController() {
-        DispatchQueue.main.async {
-            let navigationController = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController
-            navigationController?.popViewController(animated: true)
-        }
-    }
-    
-    func pushToSignUpViewController(from provider: SignInProvider) {
-        DispatchQueue.main.async {
-            let navigationController = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController
-            guard let signUpViewController = self.mainStoryboard.instantiateViewController(
-                identifier: "SignUpViewController") as? SignUpViewController else {
-                    print(String(describing: SignUpViewController.self))
-                    return
-            }
-            signUpViewController.provider = provider.rawValue
-            navigationController?.pushViewController(signUpViewController, animated: true)
-        }
-    }
-    
-    func pushToBeeMainViewController() {
-        DispatchQueue.main.async {
-            let navigationController = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController
-            guard let beeMainViewController = self.mainStoryboard.instantiateViewController(
-                identifier: "BeeMainViewController") as? BeeMainViewController else {
-                    print(String(describing: BeeMainViewController.self))
-                    return
-            }
-            navigationController?.pushViewController(beeMainViewController, animated: true)
-        }
-    }
-    
-    func pushToMemberViewController() {
-        DispatchQueue.main.async {
-            let navigationController = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController
-            guard let memberViewController = self.mainStoryboard.instantiateViewController(
-                identifier: "MemberViewController") as? MemberViewController else {
-                    print(String(describing: MemberViewController.self))
-                    return
-            }
-            navigationController?.pushViewController(memberViewController, animated: true)
-        }
-    }
-    
-    func pushToCalenderViewController() {
-        DispatchQueue.main.async {
-            let navigationController = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController
-            guard let calendarViewController = self.mainStoryboard.instantiateViewController(
-                identifier: "CalendarViewController") as? CalendarViewController else {
-                    print(String(describing: CalendarViewController.self))
-                    return
-            }
-            navigationController?.pushViewController(calendarViewController, animated: true)
-        }
-    }
-    
-    func pushToBeforeJoinViewController() {
-        DispatchQueue.main.async {
-            let navigationController = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController
-            guard let beforeJoinViewController = self.mainStoryboard.instantiateViewController(
-                identifier: "BeforeJoinViewController") as? BeforeJoinViewController else {
-                    print(String(describing: BeforeJoinViewController.self))
-                    return
-            }
-            navigationController?.pushViewController(beforeJoinViewController, animated: true)
-        }
-    }
-    
-    func pushToBeeCreateNameViewController() {
-        DispatchQueue.main.async {
-            let navigationController = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController
-            guard let beeCreateNameViewController = self.mainStoryboard.instantiateViewController(
-                identifier: "BeeCreateNameViewController") as? BeeCreateNameViewController else {
-                    print(String(describing: BeeCreateNameViewController.self))
-                    return
-            }
-            navigationController?.pushViewController(beeCreateNameViewController, animated: true)
-        }
-    }
-    
-    func pushToMissionCreateViewController() {
-        DispatchQueue.main.async {
-            let navigationController = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController
-            guard let missionCreateViewController = self.mainStoryboard.instantiateViewController(
-                identifier: "MissionCreateViewController") as? MissionCreateViewController else {
-                    print(String(describing: MissionCreateViewController.self))
-                    return
-            }
-            navigationController?.pushViewController(missionCreateViewController, animated: true)
-        }
-    }
-    
-    func pushToInvitedViewController() {
+    static func pushToInvitedViewController() {
         DispatchQueue.main.async {
             let navigationController = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController
             guard let invitedViewController = self.mainStoryboard.instantiateViewController(
